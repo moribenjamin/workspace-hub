fff00add5db899de89a67954a7981100e62c2376
Mori Benjamin <moribenjamin@icloud.com>
2017-12-06 13:11:32 -0500

    mori and mary created filters

--- src/main/java/workspacehub/ParkingRepository.java
+++ src/main/java/workspacehub/ParkingRepository.java
@@ -1,7 +1,11 @@
 package workspacehub;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ParkingRepository extends CrudRepository<Parking, Long> {
+
+	List<WorkspaceHub> findByParking(String parking);
 
 }
