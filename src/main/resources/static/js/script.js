2b6f62ac22e3def8066ff01be7bd89b672e65d2a
Sarah Strange <strange1120@gmail.com>
2017-12-11 12:17:37 -0500

    more cleanup and created location-script

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -26,10 +26,28 @@ function myFunction(){
 	}
 }
 
+<<<<<<< Updated upstream
 var myCenter;
 function initMap() {
 		// Create a styles array to use with the map.
 
+=======
+var map;
+
+var markers = [];
+
+var polygon = null;
+var locations = [];
+function store_locations(lat, lng) {
+}
+var address;
+var name;
+var idW;
+
+var myCenter;
+function initMap() {
+		
+>>>>>>> Stashed changes
 		var styles = [ {
 			featureType : 'water',
 			stylers : [ {
@@ -99,6 +117,7 @@ function initMap() {
 				lightness : -25
 			} ]
 		} ];
+<<<<<<< Updated upstream
 
 		var largeInfowindow = new google.maps.InfoWindow();
 
@@ -232,3 +251,151 @@ function initMap() {
 									return markerImage;
 								}					 		
 							 		
+=======
+		
+		map = new google.maps.Map(document.getElementById('map'), {
+			center : {
+				lat : 39.9612,
+				lng : -82.9988
+			},
+			zoom : 13,
+			styles : styles,
+			mapTypeControl : false
+		});
+		var largeInfowindow = new google.maps.InfoWindow();
+		
+		var defaultIcon = makeMarkerIcon('0091ff');
+		
+		var highlightedIcon = makeMarkerIcon('FFFF24');
+		
+var geocoder = new google.maps.Geocoder();
+
+for (var i =0; i<data.length; i++) {
+	address = data[i].address;
+	name = data[i].name;
+	idW = data[i].id;
+	console.log(idW);
+
+	geocoder
+			.geocode(
+					{
+						'address' : address,
+						componentRestrictions : {
+							locality : 'Columbus'
+						}
+					},
+					function(results, status) {
+						if (status == google.maps.GeocoderStatus.OK) {
+							locations.push(results[0].geometry.location);
+						myCenter = results[0].geometry.location;
+							
+						var position = myCenter;
+							// Create a marker per location, and put into markers array.
+							var marker = new google.maps.Marker({
+								position : position,
+								name : name,
+								animation : google.maps.Animation.DROP,
+								icon : defaultIcon,
+								url : `location?id=${idW} `,
+								id: i
+							});
+					 		markers.push(marker);
+marker.addListener('mouseover', function() {
+	populateInfoWindow(this, largeInfowindow);
+});
+// Two event listeners - one for mouseover, one for mouseout,
+// to change the colors back and forth.
+marker.addListener('mouseover', function() {
+	this.setIcon(highlightedIcon);
+});
+marker.addListener('mouseout', function() {
+	this.setIcon(defaultIcon);
+});
+ google.maps.event.addListener(marker, 'click', function() {
+        window.location.href = marker.url;
+    });
+					 		console.log(markers.length);
+					 		console.log(locations);
+						}
+						 /*  else {
+								 	alert('Geocode was not successful for the following reason: ' + status);
+						}  */
+					});
+	google.maps.event.addDomListener(window, 'load', initMap);
+	
+function populateInfoWindow(marker, infowindow) {
+	// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+		// Clear the infowindow content to give the streetview time to load.
+		infowindow.setContent('');
+		infowindow.marker = marker;
+		
+		infowindow.addListener('closeclick', function() {
+			infowindow.marker = null;
+		});
+		var streetViewService = new google.maps.StreetViewService();
+		var radius = 50;
+		
+		function getStreetView(data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+              var nearStreetViewLocation = data.location.latLng;
+              var heading = google.maps.geometry.spherical.computeHeading(
+                nearStreetViewLocation, marker.position);
+                infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+                var panoramaOptions = {
+                  position: nearStreetViewLocation,
+                  pov: {
+                    heading: heading,
+                    pitch: 30
+                  }
+                };
+              var panorama = new google.maps.StreetViewPanorama(
+                document.getElementById('pano'), panoramaOptions);
+            } else {
+              infowindow.setContent('<div>' + marker.title + '</div>' +
+                '<div>No Street View Found</div>');
+            }
+          }
+          // Use streetview service to get the closest streetview image within
+          // 50 meters of the markers position
+          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+          // Open the infowindow on the correct marker.
+          infowindow.open(map, marker);
+        }
+      }
+}
+}
+document.getElementById('show-listings').addEventListener('click',
+	showListings);
+document.getElementById('hide-listings').addEventListener('click',
+hideListings);
+
+function showListings() {
+	var bounds = new google.maps.LatLngBounds();
+	// Extend the boundaries of the map for each marker and display the marker
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+		bounds.extend(markers[i].position);
+		markers[i].url = `location?id=${i+1}`
+		markers[i].title  = data[i].name;
+	}
+	map.fitBounds(bounds)
+}
+// This function will loop through the listings and hide them all.
+function hideListings() {
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(null);
+	}
+}
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+	var markerImage = new google.maps.MarkerImage(
+			'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'
+					+ markerColor + '|40|_|%E2%80%A2', new google.maps.Size(21,
+					34), new google.maps.Point(0, 0), new google.maps.Point(10,
+					34), new google.maps.Size(21, 34));
+	return markerImage;
+}
+>>>>>>> Stashed changes
