c144e824c1b1f453c4818e079fb24a59beb4a2e0
Mori Benjamin <moribenjamin@icloud.com>
2017-12-11 10:23:08 -0500

    added about us

--- src/main/java/workspacehub/WorkSpaceController.java
+++ src/main/java/workspacehub/WorkSpaceController.java
@@ -56,7 +56,7 @@ public class WorkSpaceController {
 	@RequestMapping("/spaceType")
 	public String getOneSpaceType(@RequestParam Long id, Model model) {
 		model.addAttribute("spaceType", spaceTypeRepo.findOne(id));
-		model.addAttribute("spaceTypes", spaceTypeRepo.findAll());
+		model.addAttribute("spaceTypes", 0.findAll());
 		return "spaceType";
 	}
 
@@ -105,4 +105,10 @@ public class WorkSpaceController {
 		return "cost-parking";
 
 	}
+
+	@RequestMapping("/about-us")
+	public String aboutUs() {
+		return "about-us";
+	}
+
 }
